24bda25be5621cc8d6792fd321e1bf55b8817a53
Owen <oreed2@jhu.edu>
2021-10-23 18:35:20 -0400

    Testing: more training...

--- MITBiasMLImplementation_SVD.py
+++ MITBiasMLImplementation_SVD.py
@@ -6,25 +6,19 @@ run code with $py -3.8 MITBiasMLImplementation.py
 
 
 import numpy as np
-import tensorflow as tf
 import pandas as pd
 import re
 import nltk
 import pickle
-import matplotlib.pyplot as plt
-import seaborn as sns 
-#import warnings
 import os
 import argparse
 import random
-import numpy as np
-import tflearn
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.estimator import regression
 from statistics import median, mean
 from collections import Counter
 from numpy.random import choice
-
+import scipy
+import matplotlib.pyplot as plt
+from numpy import random
 
 
 class DataProcessor(object):
@@ -33,35 +27,39 @@ class DataProcessor(object):
 		Mode: 
 			allows for different reference data to be pulled from pickel rather than processed
 			0 - runs all processing from beggining 
-			1 - skips to csv files being input
-			2 - skips to process 
-			3 - skips to trained models (WIP)
+			1 - skips to loading csv files
+			2 - skips to loading process 
+			3 - skips to loading results array
+
 		"""
-		#data setup
 		self.mode = mode 
 		self.targetDS = targetDS
 		self.folder = folder
+		self.r = 3
+		#data setup
+		self.data_init()
+		#run code
+		self.main()
+
+	def main(self):
+		self.outputs = self.optimize_handeler(self.mode)
+		self.display_datasets()
+		"""
+		TODO:
+			impliment printout function
+				turns cols 1 and 2 of U and V into datapoints 
+		"""
+
+	def data_init(self):
+		"""
+		TODO:
+			modify original data storage to store list of news sources and list of terms
+				store this data
+		"""
 		self.dataArrayRaw = self.setup(self.folder,self.mode) 
 		self.dataArray = self.process(self.dataArrayRaw,self.mode)
 		if self.targetDS == None:
 			self.targetDS = [x for x in range(len(self.dataArray))]
-		self.dummyModelList = self.buildNetworks(self.dataArray,self.mode,self.targetDS)
-		#train models
-		self.trainedModelList = self.trainNetworks(self.dummyModelList,self.dataArray,self.targetDS,self.mode)
-		#run models
-		self.main()
-
-	def main(self):
-		dsPointer=0
-		for x in range(len(self.dataArray)):
-			if x in self.targetDS:
-				data_holder = self.trainedModelList[dsPointer].predict(np.array(self.dataArray[x][0]).reshape(len(self.dataArray[x][0]),1,1))
-				dsPointer+=1
-		with open('Data_Print_Out.txt', 'w') as f:
-			for idx in range(len(data_holder)):
-				f.write("Phrase: " + str(idx) +"\n"+ str(data_holder[idx])+"\n")
-		f.close()
-
 
 	def setup(self,folder,mode): #opens and sets up csvs
 		if mode == 0:
@@ -79,111 +77,208 @@ class DataProcessor(object):
 	def process(self,dataArrayRaw,mode):#converts csvs to python list for easier ann training
 		if mode < 2: #if we need to make a new data array to use for training 
 			dataArray = []
-			for x in range(len(dataArrayRaw)):#create an array in which each index is an array of 2 arrays (x, y's)
-				dataArray.append([[],[]]) 
 			# print(dataArray)
 			for x in range(len(dataArrayRaw)): #loop through each of our datasets
-				dataArrayRaw[x] = dataArrayRaw[x].drop(["TOTAL","PHRASE"],axis=1)#drop non data columns
-				#print(dataArrayRaw[x])
-				for y in range(0,dataArrayRaw[x].shape[0]):#add the x vals and the corrisponding source distributions for each x
-					dataArray[x][0].append(y)
-					dataArray[x][1].append(list(dataArrayRaw[x].loc[y]))
-				# print(dataArray[x][0][0],dataArray[x][0][1])
-			# print(dataArray)
+				tempP = np.array(dataArrayRaw[x]["PHRASE"])
+				tempN = np.array(dataArrayRaw[x].drop(["TOTAL","PHRASE"],axis=1))#drop non data columns
+				tempS = np.array(dataArrayRaw[x].columns)[2:]
+				# print(tempN)
+				# print(tempP)
+				# print(tempS)	
+				dataArray.append((tempN,tempP,tempS))
 			pickle.dump(dataArray, open( "processedData.p", "wb" ) )
 			return dataArray
-		if mode == 2:
+		if mode >= 2:
 			print("CSV Processing Loaded \n")
 			return pickle.load( open( "processedData.p", "rb" ) )
-		if mode > 2: 
-			print("CSV Processing Skipped \n")
+		# if mode > 2: 
+		# 	print("CSV Processing Skipped \n")
+		# 	return None
+
+	"""
+	takes a numerical input 
+	returns the ReLU of it
+	"""
+	def SVD_ReLU(self,inp):
+		if inp >= 0:
+			return inp
+		else:
 			return None
 
-	def buildNetworks(self,dataArray,mode,dsIndex=None): #builds a blank ANN for each data set
-		#ds is an array that holds n 
-		#creates an ann of input 1 (the word/ col) and output of len i (word column of news sources appearences)
-		if dsIndex == None:
-			dummyModelList = [self.neural_network_model(1, (len(ds[1][0]))) for ds in dataArray]
-			#pickle.dump(dummyModelList, open("dummyModelList.p", "wb" ) )
-		else:
-			dummyModelList = []
-			for x in range(len(dataArray)):
-				if x in dsIndex:
-					dummyModelList.append(self.neural_network_model(1, (len(dataArray[x][1][0]))))
-			#pickle.dump(dummyModelList, open("dummyModelList.p", "wb" ) )
-		return dummyModelList
+	#takes a 1d flattened index and converts to a 2d index
+	def convert_1d_2d(self,rows,cols,idx):
+		row_idx = idx//cols
+		col_idx = idx%cols
+		return row_idx, col_idx
 
-	def neural_network_model(self, input_size, output_size): #generates the actual ANN
-	    # print("Building Model...\n")
-	    LR = 1e-3
+	#flattens a 2d index into a 1d index
+	def convert_2d_1d(self,cols,idx_r,idx_c):
+		return idx_r*cols + idx_c
 
-	    network = tf.keras.models.Sequential()
-	    network = input_data(shape=(1,1), name='input') #takes in the board state as an input
-		
-	    network = fully_connected(network, 128, activation='relu') #hidden ANN layer 1 all hidden layers use relu
-	    network = dropout(network, 0.8) #dropout 80% of all data at a given level to decrease flexibilty and increase generalization
-	    # network = dropout(network, 0.95)
-	
-	    network = fully_connected(network, 256, activation='relu') #hidden ANN layer 2
-	    network = dropout(network, 0.8)
-	    # network = dropout(network, 0.95)
-	
-	    network = fully_connected(network, 512, activation='relu') #hidden ANN layer 3
-	    network = dropout(network, 0.8)
-	    # network = dropout(network, 0.95)
-	
-	    network = fully_connected(network, 512, activation='relu') #hidden ANN layer 4
-	    network = dropout(network, 0.8)
-	    # network = dropout(network, 0.95)
-	
-	    network = fully_connected(network, 256, activation='relu') #hidden ANN layer 5
-	    network = dropout(network, 0.8)
-	    # network = dropout(network, 0.95)
-	
-	    network = fully_connected(network, 128, activation='relu') #hidden ANN layer 6
-	    network = dropout(network, 0.8)
-	    #network = dropout(network, 0.95)
-	
-	    network = fully_connected(network, output_size, activation='softmax') #prints out an output matrix of probability for each given move
-	    # network = fully_connected(network, gamesize**2, activation='relu')
-	
-	    #defines methods of calculus to use for regression
-	    network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
-	    model = tflearn.DNN(network, tensorboard_dir='log')
-	    print(output_size)
-	    return model
+	"""
+	takes r value, index i and j, then W U and V matricies 
+	returns N_bar[i][j]
+	"""
+	def idx_n_bar_calc(self,r, i, j, W, U, V):
+		raw_n_bar = 0
+		for k in range(r):
+			raw_n_bar+= W[k]*U[i][k]*V[j][k]
+		return self.SVD_ReLU(raw_n_bar)
 
-	def trainNetworks(self,dummyModelList,dataArray,dsIndex,mode):
-		#need a way to only access the parts of dataArray that corrispond with what exists in dummy model list
-		#sudo: dummyList[x].fit(data[f(x)][0],data[f(x)][1]) 
-		sess = tf.compat.v1.Session()
-		saver = tf.compat.v1.train.Saver()
-		with sess:
-			if mode < 3:
-				dsPointer = 0 #allows us to increment through data array and dummyModelListSeperatly
-				#this is important because dataArray is len n wheras dummyModelList is len dsIndex
-				for x in range(len(dataArray)):
-					if x in dsIndex:
-						# print(dataArray[x][0])
-						y = np.array([np.array(dataPoint).reshape(len(dataPoint)) for dataPoint in dataArray[x][1]])
-						print(y.shape)
-						print(dummyModelList[dsPointer])
-						print(x)
-						dummyModelList[dsPointer].fit({'input': np.array(dataArray[x][0]).reshape(len(dataArray[x][0]),1,1)}, {'targets': y}, n_epoch=10, snapshot_step=500, show_metric=True, run_id='openai_learning')
-						dsPointer+=1
-				# for x in dsIndex:
-					# saver.save(sess, "./model_"+str(x))
-				# print (dummyModelList)
-				return dummyModelList
-			if mode == 3:
-				print("ANN Training Loaded \n")
-				for x in dsIndex:
-					modelList = [saver.restore(sess, "./model_"+str(x))]
-				return 
-			if mode > 3:
-				print("ANN Training Skipped \n")
-				return None
+	def parse_inputs(self,inp):
+		"""
+		first r inputs 
+			W matrix 
+		next N
+		"""
+		N = self.N
+		r = self.r #initialize r to 3 because of MIT article
+		n = (self.N.shape)[1] #n = cols --> corrisponds with i/media source
+		m = (self.N.shape)[0] #m = rows --> corrisponds with j/phrase 
+		W = np.zeros(r)
+		U = np.zeros([n,r])
+		V = np.zeros([m,r])
+		for idx in range(len(inp)):
+			if idx <r:
+				W[idx] = inp[idx]
+			elif idx<r+n*r:
+				idx_r, idx_c = self.convert_1d_2d(n,r,idx-r)#ignores the first r indicies meant for W 
+				U[idx_r][idx_c] = inp[idx]
+			else:
+				idx_r, idx_c = self.convert_1d_2d(m,r,idx-r-n*r)#ignores the first r + n*r indicies meant for W and U
+				V[idx_r][idx_c] = inp[idx]
+		return W,U,V
 
-DataProcessor("./phrasebias_data/phrase_counts",2,[1])
+	def rosen_der(self,x):
+		xm = x[1:-1]
+		xm_m1 = x[:-2]
+		xm_p1 = x[2:]
+		der = np.zeros_like(x)
+		der[1:-1] = 200*(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
+		der[0] = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
+		der[-1] = 200*(x[-1]-x[-2]**2)
+		return der
+	def rosen_hess(self,x):
+	    x = np.asarray(x)
+	    H = np.diag(-400*x[:-1],1) - np.diag(400*x[:-1],-1)
+	    diagonal = np.zeros_like(x)
+	    diagonal[0] = 1200*x[0]**2-400*x[1]+2
+	    diagonal[-1] = 200
+	    diagonal[1:-1] = 202 + 1200*x[1:-1]**2 - 400*x[2:]
+	    H = H + np.diag(diagonal)
+	    return H
+
+	"""
+	takes input of initialized W U V and original N matrix
+	returns optimized U and V matricies 
+
+	W = len r array 
+	U = n rows r cols
+	V = m rows r cols
+	N = m rows n cols
+
+	"""
+	def LnLpoi(self,inp):
+		W,U,V = self.parse_inputs(inp)
+		print(U)
+		N = self.N
+		r = self.r #initialize r to 3 because of MIT article
+		n = (self.N.shape)[1] #n = cols --> corrisponds with i/media source
+		m = (self.N.shape)[0] #m = rows --> corrisponds with j/phrase 
+		N_bar = np.zeros([m,n])
+		LnLpoi_val = 0
+		for i in range(n):
+			for j in range(m):
+				N_bar[j][i] = self.idx_n_bar_calc(r,i,j,W,U,V)
+				# if (N_bar[j][i] == None):
+				# 	# print(N_bar[j][i])
+				# 	# print("HIT")
+				# 	return None
+				LnLpoi_val_log = np.log(N[j][i]/(np.e*N_bar[j][i]))
+				if np.isinf(LnLpoi_val_log):
+				# if (LnLpoi_val_log< -100000000):
+					# print(LnLpoi_val_log)
+					# print(N[j][i],N_bar[j][i])
+					# print(N)
+					# print(U[i])
+					# print(V[j])
+					# print(W)
+					# exit()
+					LnLpoi_val += N_bar[j][i]
+				else:	
+					LnLpoi_val += (N_bar[j][i] + N[j][i]*np.log(N[j][i]/(np.e*N_bar[j][i])))
+				# print(np.log(N[j][i]/(np.e*N_bar[j][i])))
+				# print(LnLpoi_val)
+		# print(N_bar[10][5],N[10][5])
+		# print(LnLpoi_val)
+		# print(W)
+		print(N_bar,N)
+		print("\n")
+		return LnLpoi_val 
+
+	def optimize_handeler(self,mode):
+		if mode <3:
+			# outputs = [[] for x in range(len(self.dataArray))]
+			outputs = pickle.load( open( "SVD_Outputs.p", "rb" ) )
+			for DS in self.targetDS:
+				self.N = self.dataArray[DS][0]
+				# print(self.N.shape)
+				n = (self.N.shape)[1] #n = cols --> corrisponds with i/media source
+				m = (self.N.shape)[0] #m = rows --> corrisponds with j/phrase
+				r = self.r
+				# inp = np.random.rand(r + n*r + m*r)
+				inp = np.ones(r + n*r + m*r)
+				# for index in range(len(inp)):
+					# inp[index] = random.randint(500)
+					# inp[index] = 50
+				#,jac=self.rosen_der, hess=self.rosen_hess
+				out = scipy.optimize.minimize(self.LnLpoi,inp,method='Nelder-Mead',options={'xatol':1, 'maxiter':1000*len(inp),'disp': True})
+				print(out)
+				W,U,V = self.parse_inputs(out.x)
+				print("U Matrix of "+ str(DS) + ":\n")
+				print(U)
+				print("\nV Matrix of " + str(DS) + ":\n")
+				print(V)
+				outputs[DS] = [W,U,V]
+			pickle.dump(outputs, open( "SVD_Outputs.p", "wb" ) )
+			return outputs
+		if mode == 3:
+			print("SVD Processing Loaded \n")
+			return pickle.load( open( "SVD_Outputs.p", "rb" ) )
+		if mode > 3: 
+			print("SVD Processing Skipped \n")
+			return None
+	def display_datasets(self):
+		for DS in self.targetDS:
+			U = self.outputs[DS][1]
+			V = self.outputs[DS][2]
+			#plot news sources
+			y = U[:,1]
+			x = U[:,2]
+			n = self.dataArray[DS][2]
+			fig, ax = plt.subplots()
+			ax.scatter(x, y)
+			for i, txt in enumerate(n):
+				ax.annotate(txt, (x[i], y[i]))
+			plt.title("Topic" + str(DS))
+			plt.show()
+			#plot topics
+			y = V[:,1]
+			x = V[:,2]
+			n = self.dataArray[DS][1]
+			fig, ax = plt.subplots()
+			ax.scatter(x, y)
+			for i, txt in enumerate(n):
+				ax.annotate(txt, (x[i], y[i]))
+			plt.title("Topic" + str(DS))
+			plt.show()
+
+
+
+
+
+
+
+DataProcessor("./phrasebias_data/phrase_counts",2,[20])
 		
 		
